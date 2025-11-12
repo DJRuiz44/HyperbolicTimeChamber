a71311069e9ff390b2acc331c7cac354ff90f4d4
DJRuiz44 <djruiz44@gmail.com>
2025-11-11 21:00:18 -0500

    First one of the long haul.  Strings recap / review of fcc lessons

---
+++ strings.js
@@ -0,0 +1,14 @@
+let exampleStr = "Hello World";
+let firstLetter = exampleStr[0];
+
+console.log(exampleStr);
+console.log("First letter of exampleStr: " + firstLetter);
+
+console.log(typeof exampleStr);
+console.log("Using substring method");
+console.log(exampleStr.substring("World"));
+
+
+for(let i = 0; i<exampleStr.length; i++){
+    console.log(exampleStr[i]);
+}
