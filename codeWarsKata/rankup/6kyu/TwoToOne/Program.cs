161014c4b4a81303fbc974a7c55f60dcaa2c6fba
DJRuiz44 <djruiz44@gmail.com>
2025-12-24 13:12:55 -0500

    Distinct letter

---
+++ codeWarsKata/rankup/6kyu/TwoToOne/Program.cs
@@ -0,0 +1,44 @@
+﻿string TwoToOne(string stringA, string stringB)
+{
+    Dictionary<char, int> counter = new Dictionary<char, int>();
+    char[] sortedA = stringA.ToCharArray();
+    char[] sortedB = stringB.ToCharArray();
+
+
+
+    foreach (char letter in stringA)
+    {
+        Console.WriteLine("fking");
+        if (counter.ContainsKey(letter))
+        {
+            counter[letter] += 1;
+        }
+        else
+        {
+            counter[letter] = 1;
+        }
+    }
+    foreach (char letter in stringB)
+    {
+        Console.WriteLine("fking");
+        if (counter.ContainsKey(letter))
+        {
+            counter[letter] += 1;
+        }
+        else
+        {
+            counter[letter] = 1;
+        }
+    }
+
+    string returnStr = "";
+    foreach (char key in counter.Keys.OrderBy(c => c))
+    {
+        returnStr += key;
+        Console.WriteLine($"{key}: {counter[key]}");
+    }
+    Console.WriteLine(counter.Count);
+    return returnStr;
+}
+string result = TwoToOne("aretheyhere", "yestheyarehere"); //aehrsty
+Console.WriteLine(result);
