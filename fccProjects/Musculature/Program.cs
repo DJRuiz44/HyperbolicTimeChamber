9e97656e42c6157319690d2837609b4e5a1df70f
DJRuiz44 <djruiz44@gmail.com>
2025-12-27 22:58:07 -0500

    Start of Musculature class

---
+++ fccProjects/Musculature/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Muscle Shoulder = new Muscle("Supraspinatus", "Supraspinous process of the greater tubercle of the humerus", "Superior facet of the greater tubercle of the humerus", "First 10-15 degrees of abduction", "C5-C6 SupraScapular Nerve");
+Shoulder.Print();
+class Muscle
+{
+    public string Name { get; set; }
+    public string Origin { get; set; }
+    public string Insertion { get; set; }
+    public string Action { get; set; }
+
+    public string Innervation { get; set; }
+
+    public void Print()
+    {
+        Console.WriteLine($"Muscle Name:\t\t{Name}");
+        Console.WriteLine($"Origin:\t\t{Origin}");
+    }
+
+    public Muscle(string name, string origin, string insertion, string action, string innervation)
+    {
+        Name = name;
+        Origin = origin;
+        Insertion = insertion;
+        Action = action;
+        Innervation = innervation;
+    }
+}
