4403db654d302d1ce0c293aa7fbb7a0f0ce8e537
DJRuiz44 <djruiz44@gmail.com>
2026-01-07 14:30:58 -0500

    String formatting

---
+++ fccProjects/C#Fundamentals/StringFormatPractice/Program.cs
@@ -0,0 +1,9 @@
+﻿string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+string resultTwo = string.Format("{0} {0} {0}", first, second);
+Console.WriteLine(result);
+Console.WriteLine(resultTwo);
+
+decimal profits = 1000000000m;
+Console.WriteLine($"{profits:N} were made from covered calls in your retirement years :_)");
